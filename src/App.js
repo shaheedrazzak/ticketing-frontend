11879440618ab92117049a1cbaff6cee4c8f8ef1
Shaheed Razzak <shaheedrazzak@Shaheeds-MacBook-Air.local>
2024-12-12 03:55:24 +0530

    Fix build script issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
+import './App.css';
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
